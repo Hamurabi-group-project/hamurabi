9e10488e34a2ba3a5098740dc469c1f512d45f4a
todd-bowers <toddbows@gmail.com>
2022-07-08 21:01:21 -0400

    commit

--- src/main/java/Methods.java
+++ src/main/java/Methods.java
@@ -1,1 +1,17 @@
+//javaimport java.util.InputMismatchException;
+//
+//public class Methods {
+//
+//    int getNumber(String message) {
+//        while (true) {
+//            System.out.print(message);
+//            try {
+//                return scanner.nextInt();
+//            }
+//            catch (InputMismatchException e) {
+//                System.out.println("\"" + scanner.next() + "\" isn't a number!");
+//            }
+//        }
+//    }
+//}
 
