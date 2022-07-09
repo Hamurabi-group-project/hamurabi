b60ce95b6cebc11946c6d89683f0c0339ae9c4e2
todd-bowers <toddbows@gmail.com>
2022-07-08 20:03:24 -0400

    Starting point

---
+++ src/main/java/hammurabi/Hammurabi.java
@@ -0,0 +1,34 @@
+package hammurabi;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Hammurabi {
+    Random rand = new Random();
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        new Hammurabi().playGame();
+    }
+
+    void playGame() {
+        int people = 100;
+        int bushels = 2800;
+        int acres = 1000;
+        int landValue = 19;
+        int year = 0;
+        boolean gameOn = true;
+        int userInput = scanner.nextInt();
+
+        System.out.println("O great Hammurabi!\n" +
+                "You are in year 1 of your ten year rule.\n" +
+                "In the previous year 0 people starved to death.\n" +
+                "In the previous year 5 people entered the kingdom.\n" +
+                "The population is now 100.\n" +
+                "We harvested 3000 bushels at 3 bushels per acre.\n" +
+                "Rats destroyed 200 bushels, leaving 2800 bushels in storage.\n" +
+                "The city owns 1000 acres of land.\n" +
+                "Land is currently worth 19 bushels per acre.");
+
+    }
+
+}
