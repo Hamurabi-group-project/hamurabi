0b709d0094cdab209337d41b42e4694b27408896
randy <randyr_1993@yahoo.com>
2022-07-10 12:58:54 -0400

    Improved some code and made changes to a code that wasn't passing a test

--- src/main/java/hammurabi/FinalMethods.java
+++ src/main/java/hammurabi/FinalMethods.java
@@ -4,17 +4,14 @@ import java.util.Random;
 public class FinalMethods {
     static Random rand = new Random();
 
-    public Integer plagueDeaths(int population) {
+    public static Integer plagueDeaths(int population) {
 
         int plagueDeaths = 0;
         boolean isPlague;
         int plagueIndex = rand.nextInt(101);
 
-        if (plagueIndex > 85) {
-            isPlague = true;
-        } else {
-            isPlague = false;
-        }
+
+        isPlague = plagueIndex > 85;
         if (isPlague) {
             plagueDeaths = (int) (0.5 * population);
         }
@@ -22,63 +19,69 @@ public class FinalMethods {
         return plagueDeaths;
     }
 
-    public Integer starvationDeaths(int population, int bushelsFedToPeople) {
+    public static Integer starvationDeaths(int population, int bushelsFedToPeople) {
 
         int starvationDeaths = 0;
         int peopleFed = bushelsFedToPeople / 20;
 
-        if (population > bushelsFedToPeople / 20) {
+        if (population > peopleFed) {
 
-            starvationDeaths = population - bushelsFedToPeople / 20;
+            starvationDeaths = population - peopleFed;
         }
 
-        return starvationDeaths;
+        return starvationDeaths; //this returns the number of deaths due to starvation
+    }
+
+    public static Boolean uprising(int population, int howManyPeopleStarved) {
+
+        return howManyPeopleStarved > population * 0.45;
     }
 
 
-    public Boolean uprising(int population, int howManyPeopleStarved) {
+    public static Integer immigrants(int population, int acresOwned, int grainInStorage) {
 
-        boolean isRevolt = false;
-
-        if (howManyPeopleStarved > population *0.45) {
-            isRevolt = true;
-        }
-
-        return isRevolt;
-    }
-
-    public Integer immigrants(int population, int acresOwned, int grainInStorage) {
         return (20 * acresOwned + grainInStorage) / (100 * population) + 1;
     }
 
-    public Integer harvest(int acres) {
+    public static Integer harvest(int acres) {
 
-        int bushels = 0;
-        int cropYieldIndex = rand.nextInt(6) + 1;
-        bushels = acres * cropYieldIndex;
+        int bushels;
+        int harvestRate = rand.nextInt(6) + 1;
+        bushels = acres * harvestRate;
         System.out.print(bushels);
 
         return bushels;
     }
 
-    public Integer grainsEatenByRats(int bushels) {
+
+    public static Integer grainEatenByRats(int bushels) {
+
+        int ratEats = 0;
+        int ratInfestRate = rand.nextInt(100);
+
+        if (ratInfestRate < 40) {
+            ratEats = ((10 + rand.nextInt(21)) * bushels) / 100;
+        }
+        System.out.println(ratEats);
+        return ratEats;
+
      //40% chance of rat infestation
      //if so, rats will eat btwn 10%-30% of your grain
      //return amount of grain eaten by rats(possibly zero)
      //no need for boolean here because this method has already been established that there's a rat infestation
 
-        double ratEats10 = 0.1;
-        double ratEats20 = 0.2;
-        double ratEats30 = 0.3;
-
-        double grainsEaten = ratEats10 * bushels;
-
-
-        return Math.toIntExact(Math.round(grainsEaten));
+//        double ratEats10 = 0.1;
+//        double ratEats20 = 0.2;
+//        double ratEats30 = 0.3;
+//
+//        double grainsEaten = ratEats10 * bushels;
+//
+//
+//        return Math.toIntExact(Math.round(grainsEaten));
         //Math.toIntExact returns solution as an integer
     }
 
-    public Integer newCostOfLand() {
+    public static Integer newCostOfLand() {
     //price of land is random, ranges from 17 to 23 bushels/acre
     //return new price for next set of decisions player has to make
     //player will need this info to buy or sell land
